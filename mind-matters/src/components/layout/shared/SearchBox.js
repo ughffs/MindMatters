09b18c616c72ac9349639ede774fd79cd78b5e76
azerbijon123 <aaron.r.gregory@gmail.com>
2020-08-10 21:16:57 +0100

    Home page modifications

---
+++ mind-matters/src/components/layout/shared/SearchBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+    return(
+        <div style={searchBoxContainer}>
+            <input 
+                className={props.className}
+                style={inputBoxStyle}
+                type="text"
+                placeholder="Search term..."
+            />
+        </div>
+    );
+};
+
+const inputBoxStyle = {
+    flex: '1',
+    border: 'none',
+    padding: '5px',
+    backgroundColor: 'white',
+    borderRadius: '3px'
+}
+
+const searchBoxContainer = {
+    display: 'flex',
+    width: '60%'
+}
+
+export default SearchBox;
--- mind-matters/src/sections/main/Main.js
+++ mind-matters/src/sections/main/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from 'components/layout/main/Header';
 import ImageDisplayer from 'components/media/ImageDisplayer';
-import MindMattersBanner from 'images/mental-health.png';
+import BlueImage from 'images/blue-background.jpg'
+import SearchBox from 'components/layout/shared/SearchBox';
 import 'style/common.css'
 
 const Main = (props) => {
@@ -9,8 +10,15 @@ const Main = (props) => {
         <React.Fragment>
             <div className="fragment-default-style">
                 <Header />
-                <div className="body-main">
-                    <ImageDisplayer image={MindMattersBanner} /> 
+                <div className="body-main" style={customBodyStyle}>
+                    <div className="search-panel" style={searchPanelWithBgImage}>
+                        <div className="search-header">
+                            <div>
+                                MindMatters.community
+                            </div>
+                        </div>
+                        <SearchBox className="home-search-box" />
+                    </div>
                 </div>            
             </div>
             <div className="fragment-default-style">
@@ -19,5 +27,17 @@ const Main = (props) => {
         </React.Fragment>
     );
 };
+
+const searchPanelWithBgImage = {
+    width: '100%',
+    height: '70%',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${BlueImage})`
+}
+
+const customBodyStyle = {
+    alignItems: 'start'
+}
 
 export default Main;
